be3ec2ff6959c8516436c433e7f9941e4089fb53
addas3 <addas3code@gmail.com>
2023-09-30 12:45:40 +0000

    Deployment commit

--- swednews/settings.py
+++ swednews/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['swed-news.herokuapp.com', 'localhost', 'swed-news-cf4db223443b.herokuapp.com', '8000-addas3-swednews-bbrdtkk2s1q.ws-eu105.gitpod.io']
 
